6c6dc778070875a26d30a6f558079079cc259885
Pierre Yan <pierreyan@college.harvard.edu>
2022-06-30 22:05:19 -0400

    updated functionality of the sidebar. added menu clicking (no animation)

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,10 +5,10 @@ import { faAngleRight, faFile, faFilePdf, faPlugCirclePlus } from '@fortawesome/
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faJsSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-const Directory = ({ directoryName }) => {
+const Directory = ({ directoryName, toggleClass, toggle }) => {
   return (
-    <div className='directory'>
-      <FontAwesomeIcon icon={faAngleRight} />
+    <div className='directory' onClick={toggle}>
+      <FontAwesomeIcon className={toggleClass} icon={faAngleRight} />
       <span> </span>
       {directoryName}
     </div>
@@ -39,20 +39,26 @@ const Sidebar = () => {
   const [navBarClass, setNavBarClass] = useState('nav-bar');
 
   const sidebarFileSystem = {
-    'Pages': [
-      ['/', <FontAwesomeIcon icon={faJsSquare} color='#4d4d4e'/>, 'Home'],
-      ['/about', <FontAwesomeIcon icon={faJsSquare} color='#4d4d4e'/>, 'About'],
-      ['/projects', <FontAwesomeIcon icon={faJsSquare} color='#4d4d4e'/>, 'Projects'],
-      ['/experience', <FontAwesomeIcon icon={faJsSquare} color ='#4d4d4e'/>, 'Experience'],
-      ['/contact', <FontAwesomeIcon icon={faJsSquare} color='#4d4d4e'/>, 'Contact']
+    'Pages': [useState('on'),
+      [
+        ['/', <FontAwesomeIcon icon={faJsSquare} color='#4d4d4e'/>, 'Home'],
+        ['/about', <FontAwesomeIcon icon={faJsSquare} color='#4d4d4e'/>, 'About'],
+        ['/projects', <FontAwesomeIcon icon={faJsSquare} color='#4d4d4e'/>, 'Projects'],
+        ['/experience', <FontAwesomeIcon icon={faJsSquare} color ='#4d4d4e'/>, 'Experience'],
+        ['/contact', <FontAwesomeIcon icon={faJsSquare} color='#4d4d4e'/>, 'Contact']
+      ]
     ],
-    'Files': [
-      ['/resume', <FontAwesomeIcon icon={faFilePdf} color='4d4d4e'/>, 'Resume'],
-      ['/transcript', <FontAwesomeIcon icon={faFilePdf} color='4d4d4e'/>, 'Transcript']
+    'Files': [useState('on'),
+      [
+        ['/resume', <FontAwesomeIcon icon={faFilePdf} color='4d4d4e'/>, 'Resume'],
+        ['/transcript', <FontAwesomeIcon icon={faFilePdf} color='4d4d4e'/>, 'Transcript']
+      ]
     ],
-    'Links': [
-      ['https://www.linkedin.com/in/pierre-yan-5625a21b6/', <FontAwesomeIcon icon={faLinkedin} color='4d4d4e'/>, 'Linkedin'],
-      ['https://github.com/Bevebage/', <FontAwesomeIcon icon={faGithub} color='4d4d4e'/>, 'Github']
+    'Links': [useState('on'),
+      [
+        ['https://www.linkedin.com/in/pierre-yan-5625a21b6/', <FontAwesomeIcon icon={faLinkedin} color='4d4d4e'/>, 'Linkedin'],
+        ['https://github.com/Bevebage/', <FontAwesomeIcon icon={faGithub} color='4d4d4e'/>, 'Github']
+      ]
     ]
   }
 
@@ -72,9 +78,17 @@ const Sidebar = () => {
         {
           Object.entries(sidebarFileSystem).map(([key, value]) => (
             <>
-              <Directory directoryName={key} />
-              <div className='files'>
-                {value.map((navLinkInfo, i) => (
+              <Directory directoryName={key} toggleClass={value[0][0]}
+                toggle={() => {
+                  if (value[0][0] === 'on') {
+                    value[0][1]('off')
+                  } else {
+                    value[0][1]('on')
+                  }
+                }}
+              />
+              <div className={`files ${value[0][0]}`}>
+                {value[1].map((navLinkInfo, i) => (
                   <>
                     {key === 'Links' ?
                     <Link key={i} toPage={navLinkInfo[0]} linkText={navLinkInfo[2]}>
