004fdf2d969b5a8d5fe2d74aca65128da4017711
Pierre Yan <pierreyan@college.harvard.edu>
2022-07-13 17:20:27 -0400

    new concept for experience page

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,59 +1,106 @@
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faMicroscope, faSchool, faUniversity } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useRef } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
-import Card from '../Card';
 import './index.scss';
 
-const cards = [
-  ['High School', 'Back'],
-  ['College', 'Back'],
-  ['High School', 'Back'],
-  ['College', 'Back'],
-  ['High School', 'Back'],
-  ['College', 'Back'],
-  ['High School', 'Back'],
-  ['College', 'Back'],
-  ['High School', 'Back'],
-  ['College', 'Back'],
-  ['High School', 'Back'],
-  ['College', 'Back'],
-  ['High School', 'Back'],
-  ['College', 'Back'],
-]
-
 const Experience = () => {
-  const cardsRef = useRef(null)
-
-  const onWheel = (e) => {
-    let container = cardsRef.current
-    let scrollPos = container.scrollLeft
-    container.scrollTo({
-      left: scrollPos + e.deltaY,
-      behaviour: 'smooth'
-    })
-  }
-
   return (
     <div className='container experience-page'>
       <div className='text-zone'>
         <h1>
           <AnimatedLetters strArray={['M', 'y', ' ', 'E', 'x', 'p', 'e', 'r', 'i', 'e', 'n', 'c', 'e']} delay={4}/>
         </h1>
-        <h2>
-          Scroll and Hover below to find out more.
-        </h2>
-        <div className='card-cont' ref={cardsRef} onWheel={onWheel}>
-          {
-            cards.map((card, i) => (
-              <React.Fragment key={`card_complex_${i}`}>
-                <Card key={`card_${i}`} title={card[0]} disc={card[1]}/>
-                {/* <div key={`arrow_${i}`} className='arrow'>
-                  <FontAwesomeIcon icon={faArrowRight}/>
-                </div> */}
-              </React.Fragment>
-            ))
-          }
+      </div>
+      <div className='timeline'>
+        <div className='timeline__event timeline__event--type3 _1'>
+          <div className='timeline__event__icon'>
+            <FontAwesomeIcon icon={faMicroscope}/>
+          </div>
+          <div className='timeline__event__date'>
+            2017 - 2018
+          </div>
+          <div className='timeline__event__content'>
+            <div className='timeline__event__title'>
+              Associate Researcher
+            </div>
+            <div className='timeline__event__description'>
+              <p>
+              Worked on a independent project to discover the association between epigenetic motifs and gene looping.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='timeline__event timeline__event--type2 _2'>
+          <div className='timeline__event__icon'>
+            <FontAwesomeIcon icon={faSchool}/>
+          </div>
+          <div className='timeline__event__date'>
+            2016 - 2020
+          </div>
+          <div className='timeline__event__content'>
+            <div className='timeline__event__title'>
+              Haddonfield Memorial High School
+            </div>
+            <div className='timeline__event__description'>
+              <p>
+              Graduated from this place, that should be an achievement enough.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='timeline__event timeline__event--type1 _3'>
+          <div className='timeline__event__icon'>
+            <FontAwesomeIcon icon={faUniversity}/>
+          </div>
+          <div className='timeline__event__date'>
+            2020 - 2024
+          </div>
+          <div className='timeline__event__content'>
+            <div className='timeline__event__title'>
+              Harvard University
+            </div>
+            <div className='timeline__event__description'>
+              <p>
+              Majoring in Computer Science / Molecular and Cellular Biology. Currently a 3.82 GPA.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='timeline__event timeline__event--type3 _4'>
+          <div className='timeline__event__icon'>
+            <FontAwesomeIcon icon={faMicroscope}/>
+          </div>
+          <div className='timeline__event__date'>
+            2017 - 2018
+          </div>
+          <div className='timeline__event__content'>
+            <div className='timeline__event__title'>
+              Associate Researcher
+            </div>
+            <div className='timeline__event__description'>
+              <p>
+              Worked on a independent project to discover the association between epigenetic motifs and gene looping.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='timeline__event timeline__event--type2 _5'>
+          <div className='timeline__event__icon'>
+            <FontAwesomeIcon icon={faSchool}/>
+          </div>
+          <div className='timeline__event__date'>
+            2016 - 2020
+          </div>
+          <div className='timeline__event__content'>
+            <div className='timeline__event__title'>
+              Haddonfield Memorial High School
+            </div>
+            <div className='timeline__event__description'>
+              <p>
+              Graduated from this place, that should be an achievement enough.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -17,3 +17,68 @@ const Projects = () => {
 }
 
 export default Projects;
+
+// import React, { useRef, useState } from 'react';
+// import AnimatedLetters from '../AnimatedLetters';
+// import Card from '../Card';
+// import './index.scss';
+
+// const cards = [
+//   ['High School', 'Back'],
+//   ['College', 'Back'],
+//   ['High School', 'Back'],
+//   ['College', 'Back'],
+//   ['High School', 'Back'],
+//   ['College', 'Back'],
+//   ['High School', 'Back'],
+//   ['College', 'Back'],
+//   ['High School', 'Back'],
+//   ['College', 'Back'],
+//   ['High School', 'Back'],
+//   ['College', 'Back'],
+//   ['High School', 'Back'],
+//   ['College', 'Back'],
+// ]
+
+// const Experience = () => {
+//   const cardsRef = useRef(null)
+//   const [hoverIcon, setHoverIcon] = useState('vis')
+
+//   const onWheel = (e) => {
+//     if (hoverIcon === 'vis') {
+//       setHoverIcon('invis')
+//     }
+
+//     let container = cardsRef.current
+//     let scrollPos = container.scrollLeft
+//     container.scrollTo({
+//       left: scrollPos + e.deltaY,
+//       behaviour: 'smooth'
+//     })
+//   }
+
+//   return (
+//     <div className='container experience-page'>
+//       <div className='text-zone'>
+//         <h1>
+//           <AnimatedLetters strArray={['M', 'y', ' ', 'E', 'x', 'p', 'e', 'r', 'i', 'e', 'n', 'c', 'e']} delay={4}/>
+//         </h1>
+//         <h2>
+//           <span>Scroll and Hover below to find out more.&nbsp;&nbsp;&nbsp;</span>
+//           <div className={`icon-scroll ${hoverIcon}`}></div>
+//         </h2>
+//         <div className='card-cont' ref={cardsRef} onWheel={onWheel}>
+//           {
+//             cards.map((card, i) => (
+//               <React.Fragment key={`card_complex_${i}`}>
+//                 <Card key={`card_${i}`} title={card[0]} disc={card[1]}/>
+//               </React.Fragment>
+//             ))
+//           }
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Experience;
